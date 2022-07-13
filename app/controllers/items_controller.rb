9e074086f46e1af8047a5d9d0cfe759ec10b4943
xyusukex <yusk090922@yahoo.co.jp>
2022-07-13 14:16:57 +0900

    itemsコントローラーの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @item = Item.all.includes(:user).order('created_at DESC')
+    @item = Item.order('created_at DESC')
   end
 
   def new
