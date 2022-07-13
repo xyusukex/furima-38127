fc9ce6805012fc8e0bcf61d5b01ced20e72d5afa
xyusukex <yusk090922@yahoo.co.jp>
2022-07-13 17:04:36 +0900

    コメントアウト削除、ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'items#index'
 
   resources :items do
-    resources :purchases, only: :create
+    # resources :purchases, only: :create
   end
   
 end
